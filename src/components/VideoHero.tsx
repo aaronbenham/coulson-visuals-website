e68a28d6801325562be45901bfb94cf8bcc1d548
Aaron Benham <aaron.benham123@gmail.com>
2025-12-19 18:26:07 +0000

    correct directory

--- src/components/VideoHero.tsx
+++ src/components/VideoHero.tsx
@@ -37,7 +37,7 @@ export default function VideoHero() {
     };
   }, []);
 
-  const videoSrc = useMemo(() => "/portfolio/Audi ad 25fps 4 1080p.mp4", []);
+  const videoSrc = useMemo(() => "/main-video/audi-s3-25fps-1080p.mp4", []);
 
   return (
     <section ref={heroRef} className="relative h-[100svh] min-h-[640px] overflow-hidden">
